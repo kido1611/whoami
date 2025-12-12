7f3d13ff5fc659bb3d9a085c54bb8f983c6a3774
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-12-13 06:15:52 +0700

    fix: enable gracefully shutdown

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 
 use axum::serve;
 use ifconfig_me::{config::AppConfig, router::setup_router, tracing::setup_tracing};
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, signal};
 use tracing::info;
 
 #[tokio::main]
@@ -20,7 +20,32 @@ async fn main() -> Result<(), anyhow::Error> {
         listener,
         router.into_make_service_with_connect_info::<SocketAddr>(),
     )
+    .with_graceful_shutdown(shutdown_signal())
     .await?;
 
     Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+c handler");
+    };
+
+    #[cfg(unix)] // This covers Linux (Podman environment)
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install SIGTERM handler")
+            .recv()
+            .await;
+    };
+
+    // Wait for either Ctrl+C (SIGINT) or SIGTERM
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    info!("🛑 Shutdown signal received, starting graceful shutdown...");
 }
